7b2f0fa35a013333d7d613d2cb95f0dac470dfa0
abhijeetmurmu1997 <abhijeetmurmu1997@gmail.com>
2020-10-06 09:16:20 +0530

    code added

---
+++ PrimeNumFunc.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int checkPrime(int n);
+int main()
+{
+    int l,u,check,i;
+    printf("enter l and u:\n");
+    scanf("%d%d",&l,&u);
+    for(i = l;i <= u;i++)
+    {
+        if(i == 1 || i == 0)
+        continue;
+        check = 0;
+        check = checkPrime(i);
+        if(check == 0)
+        {
+           printf("%d\n",i);
+        } 
+    }
+    return 0;
+}
+int checkPrime(int n)
+{
+    int flag = 0,j;
+    for (j = 2;j <= n/2; j++)
+    {
+        if(n%j == 0)
+        {
+           flag = 1;
+           break;
+        }
+    }
+    return flag;
+}
